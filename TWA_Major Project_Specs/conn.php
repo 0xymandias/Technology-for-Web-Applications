f0b2198e9382a2ffe582ed61b6b2ea2f911e57bc
0xymandias <100314947+0xymandias@users.noreply.github.com>
2022-09-25 11:04:39 +1000

    Delete TWA_Major Project_Specs directory

--- TWA_Major Project_Specs/conn.php
+++
@@ -1,7 +0,0 @@
-<!--script to establish the connection between web and DB server-->
-<?php
-   $dbConn = new mysqli("localhost", "root", "", "MedBook");
-   if ($dbConn->connect_error) {
-      die("Failed to connect to database " . $dbConn->connect_error);
-   }
-?>
